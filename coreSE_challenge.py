d5ed8f3aa31c2ecd5fbe8b05296274ed2d66ace8
Adam Hennefer <adam.hennefer@gmail.com>
2021-04-24 21:39:39 -0700

    lazy file read

--- coreSE_challenge.py
+++ coreSE_challenge.py
@@ -2,20 +2,21 @@
 
 '''
 Author: Adam Hennefer
-Date: April 14, 2021
+Date: April 24, 2021
 version:  Python 3.6
 
 To get run time stats:
 python -m cProfile coreSE_challenge.py
 
 '''
+import os
 
 print('\n   Longest Common Byte Strand:\n')
 
 
 files = ['sample.1','sample.2','sample.3','sample.4','sample.5',
 		'sample.6','sample.7', 'sample.8','sample.9','sample.10']
-glossary = {}
+byte_dict = {}
 common_bytes = []
 max_len = 0
 longest_common =  bytes()
@@ -24,11 +25,11 @@ longest_common =  bytes()
 for file_name in files:
 	offset = 0
 	with open(file_name, 'rb') as file:
+		file_size = os.path.getsize(file_name)
 		for strand in file:
 			strand_len = len(strand)
 			try:
-				prev = glossary[strand]
-				# try clause will only continue if the byte strand was in the glossary
+				prev = byte_dict[strand] 
 				if strand == longest_common:
 					common_bytes.append((file_name, offset))
 				elif strand_len > max_len:
@@ -37,8 +38,10 @@ for file_name in files:
 					common_bytes = [prev, (file_name, offset)]
 			except KeyError as e:
 				if strand_len > max_len:
-					glossary[strand] = (file_name, offset)
+					byte_dict[strand] = (file_name, offset)
 			offset += strand_len
+			if (file_size - offset) < max_len:
+				break
 
 if not common_bytes:
 	print('   No common bytes found.\n')
