2d68d456e54867f92f8ced11f38d91115dbcb055
Adam Hennefer <adam.hennefer@gmail.com>
2021-04-29 16:21:58 -0700

    cache control

--- se_challenge.py
+++ se_challenge.py
@@ -3,7 +3,7 @@
 '''
 Author: Adam Hennefer
 Date: April 24, 2021
-version:  Python 3.6
+Requires:  Python 3.6
 
 To get run time stats:
 python -m cProfile se_challenge.py
@@ -23,6 +23,10 @@ longest_common =  bytes()
 
 
 for file_name in files:
+	if common_bytes:   # clear cache smaller than largest common byte
+		#print(f"{'size of cache: ': >11} {len(cache): >9}")
+		cache = {k:v for (k, v) in cache.items() if len(k) > max_len}
+	
 	file_size = os.path.getsize(file_name)
 	offset = 0
 	with open(file_name, 'rb') as file:
@@ -40,6 +44,7 @@ for file_name in files:
 				if strand_len > max_len:
 					cache[strand] = (file_name, offset)
 			offset += strand_len
+			# lazy file read
 			if (file_size - offset) < max_len:
 				break
 
@@ -48,4 +53,4 @@ if not common_bytes:
 else:
 	print(f"{'Files': >11} {'Offset': >9}")
 	for file, offset in common_bytes:
-		print(f"{file: >11} {offset:>9}")
+		print(f"{file: >11} {offset: >9}")
